48bfc35710c94d6009e76a2ed82a5bbef7405bcf
Angel Bulnes <angel_bulnes16@yahoo.com>
2021-04-21 11:40:25 -0600

    Finalizado logica de calculadora

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Text, Alert } from 'react-native';
 import CalcButton from '../components/CalcButton';
 import { globalStyles } from '../theme';
 import colors from '../constants/colors';
 
+enum Operators {
+  sum,
+  substract,
+  multiplication,
+  division,
+}
+
 const CalculatorScreen = () => {
   const [number, setNumber] = useState('0');
   const [previousNumber, setPreviousNumber] = useState('0');
+
+  const lastOperation = useRef<Operators>();
 
   const clean = () => {
     setNumber('0');
@@ -38,9 +47,82 @@ const CalculatorScreen = () => {
     setNumber(replaceNumber);
   };
 
+  const deleteNumber = () => {
+    let newNumber = number;
+    let negative = '';
+    if (number.includes('-')) {
+      negative = '-';
+      newNumber = number.replace('-', '');
+    }
+
+    if (newNumber.length === 1) {
+      newNumber = '0';
+    } else {
+      newNumber = negative + newNumber.slice(0, -1);
+    }
+
+    setNumber(newNumber);
+  };
+
+  const changeArithmeticNumber = () => {
+    if (number.endsWith('.')) {
+      setPreviousNumber(number.slice(0, -1));
+    } else {
+      setPreviousNumber(number);
+    }
+    setNumber('0');
+  };
+
+  const btnDivide = () => {
+    changeArithmeticNumber();
+    lastOperation.current = Operators.division;
+  };
+  const btnMult = () => {
+    changeArithmeticNumber();
+    lastOperation.current = Operators.multiplication;
+  };
+  const btnSubstract = () => {
+    changeArithmeticNumber();
+    lastOperation.current = Operators.substract;
+  };
+  const btnSum = () => {
+    changeArithmeticNumber();
+    lastOperation.current = Operators.sum;
+  };
+
+  const calculate = () => {
+    const num1 = Number(previousNumber);
+    const num2 = Number(number);
+
+    switch (lastOperation.current) {
+      case Operators.sum:
+        setNumber(`${num1 + num2}`);
+        break;
+      case Operators.substract:
+        setNumber(`${num1 - num2}`);
+        break;
+      case Operators.multiplication:
+        setNumber(`${num1 * num2}`);
+        break;
+      case Operators.division:
+        if (num2 === 0) {
+          Alert.alert('División entre 0', 'No se puede dividir entre 0');
+          return;
+        }
+        setNumber(`${num1 / num2}`);
+        break;
+      default:
+        break;
+    }
+
+    setPreviousNumber('0');
+  };
+
   return (
     <View style={globalStyles.calculatorContainer}>
-      <Text style={globalStyles.smallResult}>{previousNumber}</Text>
+      {previousNumber !== '0' && (
+        <Text style={globalStyles.smallResult}>{previousNumber}</Text>
+      )}
       <Text style={globalStyles.result} numberOfLines={1} adjustsFontSizeToFit>
         {number}
       </Text>
@@ -48,35 +130,39 @@ const CalculatorScreen = () => {
       <View style={globalStyles.row}>
         <CalcButton onPress={clean} color={colors.lightGray} text="C" />
         <CalcButton onPress={numberSign} color={colors.lightGray} text="+/-" />
-        <CalcButton color={colors.lightGray} text="del" />
-        <CalcButton color={colors.orange} text="/" />
+        <CalcButton
+          onPress={deleteNumber}
+          color={colors.lightGray}
+          text="del"
+        />
+        <CalcButton onPress={btnDivide} color={colors.orange} text="/" />
       </View>
       {/* Button row */}
       <View style={globalStyles.row}>
         <CalcButton onPress={makeNumber} text="7" />
         <CalcButton onPress={makeNumber} text="8" />
         <CalcButton onPress={makeNumber} text="9" />
-        <CalcButton color={colors.orange} text="X" />
+        <CalcButton onPress={btnMult} color={colors.orange} text="X" />
       </View>
       {/* Button row */}
       <View style={globalStyles.row}>
         <CalcButton onPress={makeNumber} text="4" />
         <CalcButton onPress={makeNumber} text="5" />
         <CalcButton onPress={makeNumber} text="6" />
-        <CalcButton color={colors.orange} text="-" />
+        <CalcButton onPress={btnSubstract} color={colors.orange} text="-" />
       </View>
       {/* Button row */}
       <View style={globalStyles.row}>
         <CalcButton onPress={makeNumber} text="1" />
         <CalcButton onPress={makeNumber} text="2" />
         <CalcButton onPress={makeNumber} text="3" />
-        <CalcButton color={colors.orange} text="+" />
+        <CalcButton onPress={btnSum} color={colors.orange} text="+" />
       </View>
       {/* Button row */}
       <View style={globalStyles.row}>
         <CalcButton onPress={makeNumber} text="0" stretch />
         <CalcButton onPress={makeNumber} text="." />
-        <CalcButton color={colors.orange} text="=" />
+        <CalcButton onPress={calculate} color={colors.orange} text="=" />
       </View>
     </View>
   );
